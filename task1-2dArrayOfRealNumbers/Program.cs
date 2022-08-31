07158e55f579c8100a6e46087a8a52db71ed454a
Ryan <llryanll@yandex.ru>
2022-09-01 00:55:10 +0300

    all tasks solved

---
+++ task1-2dArrayOfRealNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+double[,] FillArrayRealNumbers (int rows, int columns, int min, int max)
+{
+    double[,] array = new double[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble() * (max-min) + min;
+        }
+    }
+    return array;
+}
+
+void PrintArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]:f2}" + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = FillArrayRealNumbers (3, 3, -100, 100);
+PrintArray(array);
+
